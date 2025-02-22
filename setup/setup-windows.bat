19cd22352a59f93134d2cd85adfa4e27314ae7e8
KittyCathy <2286431462@qq.com>
2025-02-22 13:21:59 +0800

    fixed setup(?

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -2,19 +2,24 @@
 color 0a
 cd ..
 @echo on
+
+echo i hate u hxdiscord
+
 echo Installing dependencies.
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
-haxelib install flixel-tools
-haxelib install SScript
-haxelib install hxCodec
-haxelib install tjson
-haxelib install json2object
+
+haxelib install lime 8.0.1
+haxelib install openfl 9.3.2
+haxelib install flixel 5.5.0
+haxelib install flixel-addons 3.2.1
+haxelib install flixel-tools 1.5.1
+haxelib install flixel-ui 2.5.0
+haxelib install hxcpp-debug-server 1.2.4
+haxelib install tjson 1.4.0
+haxelib install hxdiscord_rpc 1.1.1
+haxelib git SScript https://github.com/mcagabe19-stuff/SScript-7.7.0 main
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec main
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit master
+
 echo Finished!
 pause
